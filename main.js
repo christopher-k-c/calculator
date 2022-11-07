964a79daf163df423c7e3e81641b916ce3195e3b
christopher-k-c <chriscareyk@gmail.com>
2022-11-07 16:41:03 +0000

    Fifth Commit, Animation and CSS tweaks

--- main.js
+++ main.js
@@ -146,32 +146,13 @@ const resetFunc = () => {
 }
 
 
+// GSAP Animation - Test
+const timeline = gsap.timeline ({defaults: { duration: 1}})
+timeline   
+    .from('.buttonGrid',  {y: '-100%', ease: "bounce"})
+    .from('h1', {y: '-100%', ease: "bounce"}, 0)
 
 
-
-// Animations 
-
-
-// var tl = new TimelineMax({
-//     paused:true
-//   });
-//   // letter animation
-//   tl.fromTo("h1", 8, {
-//     width: "0",
-//   }, {
-//     width: "20.18em", /* same as CSS .line-1 width */
-//     ease:  SteppedEase.config(37)
-//   }, 0);
-//   // text cursor animation
-//   tl.fromTo("h1", 0.5, {
-//     "border-right-color": "rgba(255,255,255,0.75)"
-//   }, {
-//     "border-right-color": "rgba(255,255,255,0)",
-//     repeat: -1,
-//     ease:  SteppedEase.config(37)
-//   }, 0);
-  
-//   tl.play();
 
 
 
